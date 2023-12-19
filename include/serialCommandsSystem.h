6c4d701e9f7c8a4a9491e28d51d30b11d2b12ce7
Kaya <95276965+kytpbs@users.noreply.github.com>
2023-12-19 14:45:09 +0300

    add documentation to serialCommandsSystem

--- include/serialCommandsSystem.h
+++ include/serialCommandsSystem.h
@@ -24,17 +24,73 @@ class CloudSerialSystem {
         std::queue<String> printBuffer;
         bool debug;
     public:
+    /**
+     * @brief Initializes the CloudSerialSystem object.
+     * 
+     * @param cloudSerialObject Pointer to the CloudSerialSystem object that will be printed to.
+    */
     CloudSerialSystem(String* cloudSerialObject);
+    
+    /**
+     * @brief Adds a command to the serial command system.
+     * 
+     * @param commandName The name of the command.
+     * @param function A function pointer to the command's implementation.
+     * @see "command" macro in commands.h
+    */
     void addCommand(String commandName, void (*function)(CloudSerialSystem*, std::vector<String>*));
+    
+    /**
+     * @brief Checks for commands and performs corresponding actions.
+     * 
+     * @param command The command to be checked.
+    */
     void checkForCommands(String command);
+    
+    /**
+     * @brief Checks for commands from the serial interface.
+     * 
+     * This function checks for commands from the serial interface and processes them accordingly.
+     * 
+     * @param serialObject The serial object to check for commands from.
+     */
     void checkForCommands() { this->checkForCommands(*this->cloudSerialObject); };
+    
+    
+    /**
+     * Prints the specified message to the cloud output.
+     *
+     * @param message The message to be printed.
+    */
     void print(String message);
+    
+    
+    /**
+     * Prints a debug message.
+     *
+     * @param message The message to be printed.
+     * @note This function will only print the message if debug mode is enabled.
+     * @see setDebug
+    */
     void debugPrint(String message);
+    
     /**
      * @brief Handle the print queue. This should be called in the cloud loop.
     */
     void handlePrintQueue();
+    
+    /**
+     * @brief Gets the debug mode.
+     * 
+     * @return true if debug mode is enabled. false if debug mode is disabled.
+    */
     bool getDebug();
+
+    /**
+     * @brief Sets the debug mode.
+     * 
+     * @param debug true to enable debug mode. false to disable debug mode.
+    */
     void setDebug(bool debug);
 };
 #endif
