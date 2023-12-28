b066ee2586f3d73abede66fabb4f38471e6080af
Kaya <95276965+kytpbs@users.noreply.github.com>
2023-12-28 22:15:15 +0300

    better documentation

--- include/stripCommands.h
+++ include/stripCommands.h
@@ -69,24 +69,36 @@ namespace stripCommands {
 
 #ifdef ARDUINO_ARCH_ESP32
     /**
-     * @brief Creates tasks for each strip.
-     * @param isInfinite Whether or not the tasks should be infinite.
-     * @note This function should only be called once.
-     * @note This function is only available on the ESP32.
+     * @brief Create tasks for each strip
+     * @param isInfinite Whether the tasks should be infinite or not, defaults to true
+     * If infinite, the tasks will loop forever, if not, they will run once and then delete themselves
+     * The recommended value is true, as it will run the tasks forever, and will not delete them
+     * @param stackSize The stack size of the tasks
+     * @param priority The priority of the tasks
+     * @note This function should only be called once, if isInfinite is true, 
+     * or else you will get a memory leak and a crash (if you call it more than once).
      * @details This function will pin the tasks to core 1.
     */
     void createTasksForEach(bool isInfinite = true, int stackSize = 2048, int priority = 2);
 
     /**
-     * @brief Creates tasks for each strip that are not infinite.
+     * @brief Creates tasks for each strip that are not infinite and self-killing.
      * @param stackSize The stack size of the tasks.
      * @param priority The priority of the tasks.
      * @note This function will automatically kill the tasks after they are done.
-     * @note This function should only be called once.
-     * @note This function is only available on the ESP32.
      * @details This function will pin the tasks to core 1.
     */
     void _createTasks(int stackSize, int priority);
+
+    /**
+     * @brief Creates tasks for each strip that are infinite long.
+     * @param stackSize The stack size of the tasks.
+     * @param priority The priority of the tasks.
+     * @note This function will not kill the tasks after they are done, as they are infinite and will never be done.
+     * @warning This function should only be called once, or else you will get a memory leak and a crash
+     * because the same tasks will be created multiple times.
+     * @details This function will pin the tasks to core 1.
+    */
     void _createTasksInfinite(int stackSize, int priority); //! Will pin to core 1! (Core 0 is used for WiFi, and other stuff, DON'T PIN TO CORE 0!)
 #endif
 }
